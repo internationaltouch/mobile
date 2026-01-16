1bb8b342f5bb5ec9aaab674bb43b53fdf47636b0
Gary Reynolds <gary@touch.asn.au>
2026-01-17 02:25:59 +1100

    Fix duplicate import in main_navigation_view.dart
    Remove duplicate import of touchtech_competitions package.
    All linting now passes! ✅

--- packages/touchtech_core/lib/views/main_navigation_view.dart
+++ packages/touchtech_core/lib/views/main_navigation_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:touchtech_competitions/touchtech_competitions.dart';
-import 'package:touchtech_competitions/touchtech_competitions.dart';
 import 'package:touchtech_core/touchtech_core.dart';
 
 class MainNavigationView extends ConsumerStatefulWidget {
